c6fab1c730922578402b463e7b234e678c4833dd
Vijay Barai <vijaybarai666@gmail.com>
2025-08-26 08:53:45 +0100

    Initial commit – added BigMart case study template

---
+++ bigmart_case_study_template/src/metrics.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+import numpy as np
+
+def rmse(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+    return float(np.sqrt(np.mean((y_true - y_pred) ** 2)))
